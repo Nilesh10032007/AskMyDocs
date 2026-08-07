979749cefdded97a6e51e29ae8027a901bfcc10d
Nilesh10032007 <nilesh11cho@gmail.com>
2026-08-08 02:44:33 +0530

    fix: hardcode Render API URL to bypass Vercel env var issue

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'https://askmydocs-38au.onrender.com/api';
 
 export const uploadDocument = async (file, onUploadProgress) => {
     const formData = new FormData();
